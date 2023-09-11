78eb8a34736f72ce254b4848e390fe0229b45f85
Vishal Girhepunje <girhepunjevishal@gmail.com>
2023-09-11 18:57:57 +0530

    branch vishal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Community from "./components/Community/Community";
 import Curriculum from "./components/Curriculum/Curriculum";
+import News from "./components/news/News";
 
 function App() {
   return (
     <div className="App">
       <Curriculum />
       <Community />
+      <News/>
     </div>
   );
 }
