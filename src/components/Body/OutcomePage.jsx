8abefb7b8e3292956f2f2676d99e00445868952f
rishunayak <rishunayak22@email.com>
2023-09-11 18:46:50 +0530

    Navbar and User

---
+++ src/components/Body/OutcomePage.jsx
@@ -0,0 +1,19 @@
+import { Box, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+const OutcomePage = () => {
+  return (
+    <>
+       <Box maxW="1280px">
+         <Heading>Driven By Outcomes To Launch Your Career In Tech</Heading>
+
+         <Flex>
+            <Box></Box> 
+            <Box></Box>
+         </Flex>
+       </Box>
+    </>
+  )
+}
+
+export default OutcomePage
