04867fe11376588a25c9aee630c531b6326aaf96
varun8177 <varunergurala9999@gmail.com>
2023-09-12 17:05:21 +0530

    added: curriculum responsiveness

--- src/components/Curriculum/container/TextBox.jsx
+++ src/components/Curriculum/container/TextBox.jsx
@@ -2,18 +2,18 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 import CustomHeading from "../../CustomHeading";
 
-const TextBox = ({ heading, text }) => {
+const TextBox = ({ heading, text, mobile }) => {
   const lastword = heading.split(" ");
   heading = lastword.filter((item, i) => i !== lastword.length - 1);
   return (
-    <Box mt={"200px"} mb={"230px"}>
+    <Box mt={mobile ? null : "200px"} mb={mobile ? null : "230px"}>
       <Heading fontSize={"2xl"}>
         {heading.join(" ")}{" "}
         <CustomHeading underline={false} text={lastword[lastword.length - 1]} />
       </Heading>
       <Text
         fontFamily={"Open Sans, sans-serif"}
-        w={"400px"}
+        w={mobile ? "100%" : "400px"}
         fontWeight={400}
         mt={"30px"}
       >
